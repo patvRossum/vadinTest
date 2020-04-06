8e77fe6370b79a2158e9fff84b515f0434ffd9dc
patvRossum <Patrick_van_rossum@web.de>
2020-04-06 15:37:18 +0200

    set tag

---
+++ src/main/java/com/vaadin/tutorial/com/ui/MainView.java
@@ -0,0 +1,43 @@
+package com.vaadin.tutorial.com.ui;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and
+ * use @Route annotation to announce it in a URL as a Spring managed
+ * bean.
+ * Use the @PWA annotation make the application installable on phones,
+ * tablets and some desktop browsers.
+ * <p>
+ * A new instance of this class is created for every new user and every
+ * browser tab/window.
+ */
+@Route ("")
+@PWA(name = "Vaadin Application",
+        shortName = "Vaadin App",
+        description = "This is an example Vaadin application.",
+        enableInstallPrompt = true)
+@CssImport("./styles/shared-styles.css")
+@CssImport(value = "./styles/vaadin-text-field-styles.css", themeFor = "vaadin-text-field")
+public class MainView extends VerticalLayout {
+
+    public MainView(){
+       /*
+        Button  button = new Button("Click-me");
+       DatePicker datePicker = new DatePicker("Pick a Date");
+
+       HorizontalLayout layout = new HorizontalLayout(button,datePicker);
+       layout.setDefaultVerticalComponentAlignment(Alignment.END);
+
+       add(layout);
+
+       button.addClickListener(click -> add (new Paragraph("Clicked: "+ datePicker.getValue())));
+       */
+
+    }
+}
